a3c58fbacdb286cebff7b0e13ee277a66a64ed30
Theo Lorne <tlorne@c2r3p3.42perpignan.fr>
2024-05-17 19:32:20 +0200

    Petit ajout

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -25,18 +25,18 @@
 const std::string HTML_FILE_PATH = "index.html";
 
 // FONCTION TO CLEAN STRG
-void    trim(std::string& str);
-void    removeLastSemicolon(std::string& str);
-bool    containsDot(const std::string& str);
-int     lastWordaFile(std::string str);
-int     lastNotaBS(std::string str);
-void    adjust(std::string& str);
-void    split(const std::string& str, char delimiter, std::vector<std::string>& tokens);
+void        trim(std::string& str);
+void        removeLastSemicolon(std::string& str);
+bool        containsDot(const std::string& str);
+int         lastWordaFile(std::string str);
+int         lastNotaBS(std::string str);
+void        adjust(std::string& str);
+void        split(const std::string& str, char delimiter, std::vector<std::string>& tokens);
 
 
 //FONCTION TO CHOOSE THE GOOD LOCATION
-int     countCommonCharacters(const std::string &s1, const std::string &s2);
-int     findClosestStringIndex(const std::string &target, const std::vector<Location *> &vecteur);
+int         countCommonCharacters(const std::string &s1, const std::string &s2);
+int         findClosestStringIndex(const std::string &target, const std::vector<Location *> &vecteur);
 
 
 //FONCTION TO GENERATE RESPONSE
@@ -45,7 +45,7 @@ std::string loadFileContent(const std::string& filePath);
 std::string generateCSSPart(std::string filePath);
 std::string generateHTMLPart(std::string filePath);
 std::string generateImagePart(std::string filePath);
-int	lastElem(std::string str);
+int	        lastElem(std::string str);
 std::string lastExt(const std::string &str);
 std::string generateResponse(std::string filePath, std::string relativ_path);
 
